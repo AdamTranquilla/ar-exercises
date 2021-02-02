9775daf6cb0a299257d158deedd6f8e755f76a79
AdamTranquilla <adam.tranquilla@gmail.com>
2021-02-02 02:17:05 +0000

    ex 1-3 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+puts "@store1 ==> #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+puts "@store2 ==> #{@store2.name}"
+@store1.name = "randys greasy rub'n-tug"
+puts "@store1 changed to ==> #{@store1.name}"
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,9 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+puts "There are currently #{Store.count} stores"
+puts "@store3 will be destroyed ==> #{@store3.name}"
+puts "----------"
+@store3.destroy
+puts "#{Store.count} stores are left standing"
